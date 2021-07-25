650e4426aa9e549f2fd330340a726e94ca2c24b9
bimbowande <oluwagbuyi.abimbola@thegiggroupng.com>
2021-07-25 09:39:17 +0100

    update branch

--- src/components/Private/PrivateRoute.tsx
+++ src/components/Private/PrivateRoute.tsx
@@ -16,7 +16,7 @@ export const PrivateRoute = (props: PrivateRouteProps) => {
     return (
         <Route{...rest}
             render={(routeProps) =>
-                isAuthenticated !== null ? (
+                isAuthenticated == null ? (
                     <Component {...routeProps} />
                 ) : (
                         <Redirect
